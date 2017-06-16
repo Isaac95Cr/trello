2090d5cf49450db1e048a235e1e23e67f26bad16
PastorCR <isakucorrales@gmail.com>
2017-06-16 15:01:32 -0600

    login complete

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import { render } from 'react-dom';
+import './main.scss';
 import { Provider } from 'react-redux';
 import store from './store';
+import logo from './img/Trello-logo.svg'
+import user from './img/username.svg'
 
 const App = () => (
   <Provider store={store}>
-      <h1>Holi :3</h1>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-4">
+
+          <div className="login">
+            <div className="login__wrapper">
+              <div className="login__logo">
+                <img src={logo} />
+              </div>
+              <form className="login__form">
+                <input type="text" className="login__input username" />
+                <input type="text" className="login__input password" />
+                <button className="btn btn-default">Log in</button>
+              </form>
+              <div className="login__or">
+                  <hr className="hr-text" data-content="OR" />
+              </div>
+              <div className="login__create">
+                <a href="" className="">Create a Trello acount</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-8 hidden-xs">
+          <div className="home__background"></div>
+        </div>
+      </div>
+    </div>
+
   </Provider>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,36 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: [ 'bootstrap-loader', './src/app.jsx' ],
+  entry: ['./src/app.jsx', 'bootstrap-loader'],
   devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   devServer: {
-    publicPath:  path.resolve(__dirname, '/'),
+    publicPath: path.resolve(__dirname, '/'),
     historyApiFallback: true,
     inline: true,
   },
   module: {
     rules: [
       {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      },
+       test: /\.scss$/,
+       use: [
+         {
+           loader: 'style-loader' // creates style nodes from JS strings
+         },
+         {
+           loader: 'css-loader' // translates CSS into CommonJS
+         },
+         {
+           loader: 'sass-loader', // compiles Sass to CSS
+           options: {
+             includePaths: [path.join(__dirname, '/node_modules/bootstrap-sass/assets/stylesheets/bootstrap/')]
+           }
+         }
+       ]
+     },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -33,18 +43,21 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader"
       },
-      { 
-        test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
-        loader: 'imports-loader?jQuery=jquery' 
+      {
+        test: /bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
+        loader: 'imports-loader?jQuery=jquery'
       },
-
+      {
+       test: /\.(eot|ttf|svg|gif|png|jpg)$/,
+       loader: "file-loader"
+      }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.tpl.ejs',
     }),
-    new ExtractTextPlugin('dist/style.css')
+    //new ExtractTextPlugin('dist/style.css')
   ]
 
 };
